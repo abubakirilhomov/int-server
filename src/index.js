2598dd8ca02228b9e943b8a924cbebd4324d0a4c
abubakirilhomov <2ilhomovabubakir2@gmail.com>
2025-10-13 13:08:59 +0500

    asdada

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const cors = require("cors");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const app = express();
-const connectDB = require('./config/database');
-const internRoutes = require('./routes/internRoutes');
-const branchRoutes = require('./routes/branchRoutes');
-const mentorRoutes = require('./routes/mentorRoutes');
-const lessonsRoutes = require('./routes/lessonRoutes');
-const rulesRoutes = require("./routes/rulesRoutes")
+const connectDB = require("./config/database");
+const internRoutes = require("./routes/internRoutes");
+const branchRoutes = require("./routes/branchRoutes");
+const mentorRoutes = require("./routes/mentorRoutes");
+const lessonsRoutes = require("./routes/lessonRoutes");
+const rulesRoutes = require("./routes/rulesRoutes");
 
 const port = process.env.PORT || 3000;
 
@@ -16,16 +16,25 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "http://localhost:5173","http://localhost:5174", "http://localhost:5175", "https://mentors-rho.vercel.app", "https://interns-lovat.vercel.app", "https://internship-admin-zeta.vercel.app"
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:5175",
+      "https://mentors-rho.vercel.app",
+      "https://interns-lovat.vercel.app",
+      "https://internship-admin-zeta.vercel.app",
+      "https://www.interns-mars.uz",
+      "https://interns-mars.uz",
+      "https://mentors-mars.uz",
+      "https://interns-admin.uz"
     ],
   })
 );
 
-app.use('/api/interns', internRoutes);
-app.use('/api/branches', branchRoutes);
-app.use('/api/mentors', mentorRoutes);
-app.use('/api/lessons', lessonsRoutes);
-app.use('/api/rules', rulesRoutes)
+app.use("/api/interns", internRoutes);
+app.use("/api/branches", branchRoutes);
+app.use("/api/mentors", mentorRoutes);
+app.use("/api/lessons", lessonsRoutes);
+app.use("/api/rules", rulesRoutes);
 
 connectDB();
 
